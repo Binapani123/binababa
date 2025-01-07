1135f1a08bf3f42ac834d4d2103c9032fbf1575a
binapani9556 <125079858+binapani9556@users.noreply.github.com>
2025-01-08 00:04:29 +0530

    added some code

--- src/test/java/Action/gitkamlesh.java
+++ src/test/java/Action/gitkamlesh.java
@@ -2,7 +2,21 @@ package Action;
 
 public class gitkamlesh {
 	public static void main(String[] args) {
-		System.out.println("kamlesh testing");
+		String str="binapani";//scp
+		String str1="binu";// scp
+		boolean result= str==str1;//false
+		System.out.println(result);
+		
+		boolean result1= str.equals(str1);//tr
+		System.out.println(result1);
+		
+		String s=new String("jaan");
+		String s1=new String("mansi");
+		
+		Boolean results =s==s1;
+		System.out.println(results);
+		
+		
 	}
 
 }
